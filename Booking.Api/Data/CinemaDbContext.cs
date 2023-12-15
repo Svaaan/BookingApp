6c8e1b1ac96a7bb7dfa571cf22ccd8c9ce7765f3
Tricksterone <113665954+Tricksterone@users.noreply.github.com>
2023-12-15 13:33:02 +0100

    created Movie entity and DbContext

---
+++ Booking.Api/Data/CinemaDbContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Booking.Api.Entities;
+
+namespace Booking.Api.Data
+{
+    public class CinemaDbContext : DbContext
+    {
+        public CinemaDbContext(DbContextOptions _options) : base(_options) { }
+        public DbSet<Movies> movies { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movies>()
+                .Property(e => e.Genre)
+                .HasConversion(
+                v => v.ToString(),
+                v => (Genres)Enum.Parse(typeof(Genres), v));
+
+            modelBuilder.Entity<Movies>()
+                .Property(e => e.Language)
+                .HasConversion(
+                v => v.ToString(),
+                v => (Languages)Enum.Parse(typeof(Languages), v));
+
+            modelBuilder.Entity<Movies>()
+                .Property(e => e.Subtitle)
+                .HasConversion(
+                v => v.ToString(),
+                v => (Subtitles)Enum.Parse(typeof(Subtitles), v));
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Movies>().HasData(new Movies
+            {
+                ID = 2,
+                Title = "The Godfather",
+                Description = "The aging patriarch of an organized crime dynasty in postwar New York City transfers control of his clandestine empire to his reluctant youngest son.ring",
+                Director = "Francis Ford Coppola",
+                Hours = 2,
+                Minutes = 55,
+                Language = Languages.English,
+                Subtitle = Subtitles.Swedish,
+                Genre = Genres.Crime,
+                ReleaseYear = 1972,
+                AgeRestriction = 15,
+                MaxShows = 5,
+            });
+        }
+    }
+}
---
+++ Booking.Api/Entities/Movies.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Booking.Api.Entities
+{
+    public class Movies
+    {
+        public int ID { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Director { get; set; }
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+        public int ReleaseYear { get; set; }
+        public int AgeRestriction { get; set; }
+        public int MaxShows { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public Genres Genre { get; set; }
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public Languages Language { get; set; }
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public Subtitles Subtitle { get; set; }
+    }
+
+    public enum Genres
+    {
+        Romance,
+        Action,
+        Horror,
+        Adventure,
+        Family,
+        Drama,
+        Crime,
+        Fantasy,
+        Thiller
+
+    }
+
+    public enum Languages
+    {
+        English,
+        Swedish
+    }
+
+    public enum Subtitles
+    {
+        English,
+        Swedish
+    }
+}
