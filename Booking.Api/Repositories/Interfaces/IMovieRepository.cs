ab1916f753995d4a9bd127d6ab9ae9b77944d621
Tricksterone <113665954+Tricksterone@users.noreply.github.com>
2024-01-18 14:29:01 +0100

    update endpont done.

--- Booking.Api/Repositories/Interfaces/IMovieRepository.cs
+++ Booking.Api/Repositories/Interfaces/IMovieRepository.cs
@@ -6,7 +6,9 @@ namespace Booking.Api.Repositories.Interfaces
     public interface IMovieRepository
     {
         Task<Movie>CreateMovieAsync(Movie movie);
+        Task<Movie> GetMovieById(int id);
         Task<List<Movie>> GetAllMoviesAsync();
         Task<Movie> DeleteMovieByIdAsync(int Id);
+        Task<Movie> UpdateMovieById(int movieId, Movie updateMovie);
     }
 }
