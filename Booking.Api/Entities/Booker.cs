3ab852ff063780ddc8e8e3d7c8de7d52fc9a7c7f
Svaaan <daniel_svan1@hotmail.com>
2024-01-20 12:34:08 +0100

    Configured api for posting data from booking form and updated entity framework

--- Booking.Api/Entities/Booker.cs
+++ Booking.Api/Entities/Booker.cs
@@ -3,9 +3,9 @@
     public class Booker
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public int LastName { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
     }
 }
