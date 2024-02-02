66898965485e05bf9bea9f3782a29c27b5da3f4c
Tricksterone <113665954+Tricksterone@users.noreply.github.com>
2024-02-02 09:58:06 +0100

    added documentation

--- Booking.Api/Controllers/ReservationController.cs
+++ Booking.Api/Controllers/ReservationController.cs
@@ -21,7 +21,18 @@ namespace Booking.Api.Controllers
             _reservationRepository = reservationRepository;
         }
 
+        /// <summary>
+        /// Create a reservation based on the Dto inputs
+        /// </summary>
+        /// <param name="reservationDto"></param>
+        /// <returns>The reservation show</returns>
+        /// <response code="201">Returns the newly created Reservation.</response>
+        /// <response code="400">If the reservation values are not valid.</response>
+        /// <response code="500">Processing failure.</response>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<Reservation>> CreateReservation([FromBody]ReservationDto reservationDto)
         {
             if (!ModelState.IsValid)
@@ -33,7 +44,16 @@ namespace Booking.Api.Controllers
             return Ok(createReservation);
         }
 
+        /// <summary>
+        /// Lists all reservations.
+        /// </summary>
+        /// <returns>A list of reservation.</returns>
+        /// <response code="200">Returns the list of reservations.</response>
+        /// <response code="500">If an error occurs while retrieving the reservations.</response>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<List<Reservation>>> ListAllReservations()
         {
             var reservationList = await this._reservationRepository.GetAllReservations();
@@ -45,6 +65,12 @@ namespace Booking.Api.Controllers
             return Ok(reservationList);
         }
 
+        /// <summary>
+        /// Get the reservation object.
+        /// </summary>
+        /// <returns>An object of the reservation.</returns>
+        /// <response code="200">Returns the reservation object.</response>
+        /// <response code="404">Return a not found if incorrect id and/or id doesnt exist</response>
         [HttpGet("{id:int}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -60,7 +86,15 @@ namespace Booking.Api.Controllers
             return Ok(reservation);
         }
 
+        /// <summary>
+        /// Update a reservation by entering it's ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="updateReservation"></param>
+        /// <returns>Returns the updated Reservation.</returns>
         [HttpPut("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<Reservation>> UpdateReservation(int id, [FromBody] ReservationDto updateReservation)
         {
             try
@@ -87,13 +121,22 @@ namespace Booking.Api.Controllers
             }
         }
 
+        /// <summary>
+        /// Delete a reservation
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <response code="200">Return a object specifying which reservation that has been deleted</response>
+        /// <response code="404">Return a not found if incorrect id and/or id doesnt exist</response>
         [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<Reservation>> DeleteReservation(int id)
         {
             var deleteReservation = await _reservationRepository.DeleteReservation(id);
             if (deleteReservation == null)
             {
-                return BadRequest();
+                return NotFound();
             }
             return Ok(deleteReservation);
         }
