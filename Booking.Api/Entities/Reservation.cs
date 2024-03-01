c97f0c604ba0c9e9c2b94dff72e9b3d61ce5b2b3
Svaaan <daniel_svan1@hotmail.com>
2024-03-01 13:24:37 +0100

    making it remote

--- Booking.Api/Entities/Reservation.cs
+++ Booking.Api/Entities/Reservation.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public int ShowId { get; set; }
         public string BookerEmail { get; set; }
-        public int BookedSeats { get; set; }
+        public int BookedSeats { get; set; } 
         public DateTime ReservationTime { get; set; } = DateTime.UtcNow;
     }
 }
