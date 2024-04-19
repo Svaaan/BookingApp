3f1dad3aa039b6f33386a1629d40abcbee12a688
Tricksterone <113665954+Tricksterone@users.noreply.github.com>
2024-04-19 09:39:09 +0200

    Loging and logout + accessdeniedpages

--- BookerPortal.Web/ViewModels/LoginViewModel.cs
+++ BookerPortal.Web/ViewModels/LoginViewModel.cs
@@ -5,7 +5,7 @@ namespace BookerPortal.Web.ViewModels
     public class LoginViewModel
     {
         [Required(AllowEmptyStrings = false, ErrorMessage = "Please provide User Name")]
-        public string? Username { get; set; }
+        public string? Email { get; set; }
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "Please provide Password")]
         public string? Password { get; set; }
