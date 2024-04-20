d39d4b5f9db00639bde15a1fe91bf88ba0e512d3
Svaaan <daniel_svan1@hotmail.com>
2024-04-20 18:07:40 +0200

    Name change

---
+++ Booking.Api/Entities/BookingConfirmation.cs
@@ -0,0 +1,23 @@
+﻿namespace Booking.Api.Entities
+{
+    public class BookingConfirmation
+    {
+        public int ReservationId { get; set; }
+        public int ShowId { get; set; }
+        public string BookerEmail { get; set; }
+        public int BookedSeats { get; set; }
+        public DateTime ReservationTime { get; set; } = DateTime.UtcNow;
+        public decimal TotalCost { get; set; }
+
+        public BookingConfirmation(Reservation reservation, Show show)
+        {
+            ReservationId = reservation.Id;
+            ShowId = reservation.ShowId;
+            BookerEmail = reservation.Booker.Email;
+            BookedSeats = reservation.BookedSeats;
+            ReservationTime = reservation.ReservationTime;
+
+            TotalCost = show.CalculateTotalCost(reservation.BookedSeats);
+        }
+    }
+}
